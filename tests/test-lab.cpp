bfc26618dc8d7d494ffb45b7fd0e80d44316f6ae
Matt R <85653509+4runnerlife@users.noreply.github.com>
2023-04-19 22:16:57 +0000

    Added test for creating a queue. 4.19.23

--- tests/test-lab.cpp
+++ tests/test-lab.cpp
@@ -3,7 +3,14 @@
 #include <gtest/gtest.h>
 
 
-TEST(CreateAndDestroy, init)
+TEST(QueueWith0, init)
 {
-     FAIL();
+     queue_t testQ = queue_init(1);
+     //EXPECT_EQ(testQ->capacity,1);
+     //EXPECT_EQ(testQ->head,NULL)
+     //EXPECT_EQ(testQ->head,testQ->tail);
+     //EXPECT_EQ(testQ->lock,NULL)
+     //EXPECT_EQ(testQ->empty,NULL)
+     //EXPECT_EQ(testQ->full,NULL)
+
 }
